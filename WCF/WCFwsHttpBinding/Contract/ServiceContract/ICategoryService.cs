54d561aafd9e367ab7e9abda5bd490524da2748f
RockLsj <GeniusAndSharp@163.com>
2020-09-13 18:19:08 +0800

    add WCFwsHttpBinding server

---
+++ WCF/WCFwsHttpBinding/Contract/ServiceContract/ICategoryService.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.DataContracts;
+using System.ServiceModel;
+
+namespace Contracts.ServiceContracts
+{
+    [ServiceContract(Name = "CategoryService", Namespace = "http://rock.com/categoryservice")]
+    public interface ICategoryService
+    {
+        [OperationContract]
+        string GetCategoryName(int categoryID);
+
+        [OperationContract]
+        Category GetCategoryDetails(int categoryID);
+
+        [OperationContract]
+        Category GetCategoryString(string str);
+
+        [OperationContract]
+        Category GetCategorySelf(Category str);
+    }
+}
---
+++ WCF/WCFwsHttpBinding/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,50 @@
+﻿using Contracts.DataContracts;
+using Contracts.ServiceContracts;
+using System.ServiceModel;
+
+//rock lsj
+namespace Services.CategoryServices
+{
+    [ServiceBehavior(Name = "CategoryService", Namespace = "http://rock.com/categoryservice")]
+    public class CategoryService : ICategoryService
+    {
+        public string GetCategoryName(int categoryID)
+        {
+            return "GetCategoryName " + categoryID;
+        }
+
+        public Category GetCategoryDetails(int categoryID)
+        {
+            Category category = new Category();
+
+            category.CategoryID = categoryID;
+            category.CategoryName = "CategoryName " + categoryID;
+            category.CategoryDescription = "Hello " + categoryID;
+            category.CategoryURL = "baiu.com " + categoryID;
+
+            return category;
+        }
+
+        public Category GetCategoryString(string str)
+        {
+            Category category = new Category();
+            category.CategoryID = 1;
+            category.CategoryName = "1223";
+            category.CategoryDescription = "1234456";
+            category.CategoryURL = "http://northwind.com/Beverages";
+
+            return category;
+        }
+
+        public Category GetCategorySelf(Category ca)
+        {
+            //ca.CategoryID = 1;
+            //ca.CategoryName = "test";
+            //ca.CategoryDescription = "12311111114456";
+            //ca.CategoryURL = "http://northwind.com/Beverages";
+
+
+            return ca;
+        }
+    }
+}
